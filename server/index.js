2dbb3263ef6d5019ac558fad4e62d4ca0c26e359
vijay <vijayreddy6812@gmail.com>
2025-05-25 10:42:16 -0400

    updated docker issue

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ io.on('connection', socket => {
 });
 
 // Fallback: serve index.html for any other routes
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
